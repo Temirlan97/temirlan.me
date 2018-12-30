162f9a4b93396da0e76f341228e1c466f14db110
root <ulugbekuulutemirlan@gmail.com>
2018-12-30 02:39:08 +0100

    more personalization of template

--- index.py
+++ index.py
@@ -1,14 +1,17 @@
+import os
 from flask import Flask
 from flask import render_template
+from flask import url_for
+from flask import send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def index():
    return render_template('DevFolio/index.html')
-
-@app.route('/pg')
-def pg():
-   return render_template('DevFolio/index.html')
+   
+@app.route('/.well-known/pki-validation/5ED2D1296FED2CC2A0EAEC6572EFA1A8.txt')
+def ssl():
+   return app.send_static_file('5ED2D1296FED2CC2A0EAEC6572EFA1A8.txt')
 
 
 if __name__ == '__main__':
